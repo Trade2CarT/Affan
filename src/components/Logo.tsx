6b4437f8a84e2b13b8a58d3d98e5fb0b765992a2
Imran S <trade2cart@gmail.com>
2025-12-07 22:45:10 +0530

    Logo FIxed

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,61 +1,24 @@
 // src/components/Logo.tsx
 import React from 'react';
+import logoImg from '../assets/Logo.png';
 
-interface LogoProps extends React.SVGProps<SVGSVGElement> {
+interface LogoProps extends React.HTMLAttributes<HTMLDivElement> {
     className?: string;
 }
 
 const Logo: React.FC<LogoProps> = ({ className, ...props }) => {
     return (
-        <svg
-            viewBox="0 0 600 320"
-            xmlns="http://www.w3.org/2000/svg"
-            className={className}
+        <div
+            // Created a white square container with padding
+            className={`bg-white aspect-square flex items-center justify-center p-1 rounded-lg ${className || ''}`}
             {...props}
         >
-            {/* Outer border ring */}
-            <ellipse
-                cx="300"
-                cy="160"
-                rx="295"
-                ry="155"
-                fill="none"
-                stroke="black"
-                strokeWidth="2"
+            <img
+                src={logoImg}
+                alt="Affan Associates Logo"
+                className="w-full h-full object-contain"
             />
-
-            {/* White spacing layer */}
-            <ellipse
-                cx="300"
-                cy="160"
-                rx="285"
-                ry="148"
-                fill="white"
-            />
-
-            {/* Dark inner background */}
-            <ellipse
-                cx="300"
-                cy="160"
-                rx="275"
-                ry="140"
-                fill="#1a1a1a"
-            />
-
-            {/* Left 'A' */}
-            <path
-                d="M 285,55 L 285,255 L 225,255 L 225,185 L 175,185 L 160,255 L 100,255 Q 60,160 120,55 Z M 225,135 L 225,105 L 160,105 L 148,135 Z"
-                fill="white"
-            />
-
-            {/* Right 'A' (Mirrored) */}
-            <g transform="scale(-1, 1) translate(-600, 0)">
-                <path
-                    d="M 285,55 L 285,255 L 225,255 L 225,185 L 175,185 L 160,255 L 100,255 Q 60,160 120,55 Z M 225,135 L 225,105 L 160,105 L 148,135 Z"
-                    fill="white"
-                />
-            </g>
-        </svg>
+        </div>
     );
 };
 
